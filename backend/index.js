73e3b0bb47f3bcc30c28a3e1802496d0806bf403
aakashloyar <aakashloyar@gmail.com>
2025-01-28 15:16:44 +0530

    Connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose=require('mongoose')
+require('dotenv').config();
+const mongoUri=process.env.MONGO_URI;
+try{
+    mongoose.connect(mongoUri);
+    console.log('Connected to MongoDB');
+} catch(err){
+    console.log('Erro connecting to MongoDB'+err);
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
