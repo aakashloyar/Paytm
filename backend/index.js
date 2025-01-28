87f9e4380af9a1848f65a8344053204982780720
aakashloyar <aakashloyar@gmail.com>
2025-01-28 22:58:32 +0530

    Add cors, body parser and jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3000
 const mongoose=require('mongoose')
 const rootRouter=require('./routes/index.js')
+const cors=require('cors');
 require('dotenv').config();
-
-
+app.use(express.json());
+app.use(cors());
 
 const mongoUri=process.env.MONGO_URI;
 try{
