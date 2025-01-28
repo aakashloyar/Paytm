6b01b056b8e04ec4ca11e9f0da7511f71722dbab
aakashloyar <aakashloyar@gmail.com>
2025-01-29 00:40:18 +0530

    added authentication middleware

---
+++ backend/routes/middleware.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const jwt=require('jsonwebtoken')
+require('dotenv').config();
+const JWT_SECRET=process.env.JWT_SECRET;
+
+const authMiddleware=(req,res,next)=>{
+    const authHeader=req.header.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({});
+    }
+    const token = authHeader.split(' ')[1];
+    try{
+        const decoded=jwt.verify(token,JWT_SECRET);
+        req.userid=decoded._id;
+        next();
+    } catch(err) {
+        res.sendStatus(403).json({
+            message:"Not authorized"   
+        })
+    }
+
+}
+module.exports=authMiddleware;
